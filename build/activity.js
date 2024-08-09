6af9a10671adbca08e6719ed6253b4af09ddb0bf
dev <88478619+alexxnder1@users.noreply.github.com>
2024-08-09 21:00:09 +0300

    Delete build directory

--- build/activity.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const main_1 = require("./main");
-main_1.client.user.setActivity('github.com/alexxnder1', { type: discord_js_1.ActivityType.Watching });
